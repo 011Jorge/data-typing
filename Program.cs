af679923bcd978e791688a1a63ec3b52a782c94d
011Jorge <vicentejorge187@gmail.com>
2022-04-13 22:11:07 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tipagem_e_Saida_de_dados
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string nameCurso = "Sociedade do Código";
+            float ageF = 26;
+            string duploCaract = "1234L";
+            float numberFracionF = 12.1231F;
+            double numberFracionD = 142.2221D; 
+            bool simpleInformation = true;
+            char uniqueCaract = 'a';
+
+
+            Console.WriteLine(nameCurso);
+        }
+    }
+}
